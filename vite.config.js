84fd3222f458f9cbbb31d42764047dd8edcb9a76
yichienlu <yichien.lu89@gmail.com>
2023-02-20 11:33:49 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base:"/Vue2023_Week7/", // github 儲存庫名稱
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
